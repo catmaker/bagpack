ffc346019734ed8fe2d3f81886c99199ff13cece
catmaker <rzt0317@nate.com>
2024-06-06 22:47:50 +0900

    feat : card component 생성

---
+++ src/components/ui/Card.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+type CardProps = {
+  width?: number | string;
+  height?: number | string;
+  children?: React.ReactNode;
+  borderRadius?: number;
+  padding?: number;
+  boxShadow?: string;
+  border?: string;
+  backgroundColor?: string;
+};
+
+const Card = ({
+  width = 864,
+  height = 444,
+  borderRadius = 30,
+  children,
+  padding = 28,
+  boxShadow = "1px 4px 4px 5px rgba(0, 0, 0, 0.25)",
+  border = "none",
+  backgroundColor = "white",
+}: CardProps) => {
+  return (
+    <div
+      style={{
+        width: width,
+        height: height,
+        borderRadius: borderRadius,
+        padding: padding,
+        boxShadow: boxShadow,
+        border: border,
+        backgroundColor: backgroundColor,
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Card;
