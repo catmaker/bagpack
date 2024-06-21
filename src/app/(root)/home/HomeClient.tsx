24e3f3dc725f1627b1ae4abc8c8d750cc9270022
catmaker <rzt0317@nate.com>
2024-06-21 22:37:00 +0900

    🐛 로그인 직후에는 /home으로 이동하지 않는 현상 수정

--- src/app/(root)/home/HomeClient.tsx
+++ src/app/(root)/home/HomeClient.tsx
@@ -3,16 +3,19 @@ import React, { useEffect, useContext } from "react";
 import { UserContext } from "@/app/provider/UserProvider";
 import { useRouter } from "next/navigation";
 import Card from "@/components/ui/Card";
+
 const HomeClient = () => {
   const router = useRouter();
   const user = useContext(UserContext);
 
   useEffect(() => {
     console.log(user);
+    if (user && !("palette" in user)) {
+      // user가 null이 아니고, palette 필드가 없으면
+      router.push("/palette");
+    }
   }, [user]);
-  if (user?.isDone === false) {
-    router.push("/palette");
-  }
+
   return <Card>H</Card>;
 };
 
