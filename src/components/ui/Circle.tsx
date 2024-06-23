a709eac7028cd174cde29db830ff856c49c9f963
catmaker <rzt0317@nate.com>
2024-06-23 22:41:03 +0900

    ✨ Circle 컴포넌트 className Prop 추가

--- src/components/ui/Circle.tsx
+++ src/components/ui/Circle.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 type CircleProps = {
-  color: string;
+  color?: string;
   size?: number;
+  children?: React.ReactNode;
+  className?: string;
 };
-const Circle = ({ color, size = 15 }: CircleProps) => {
+const Circle = ({ color, size = 15, children, className }: CircleProps) => {
   const style = {
     backgroundColor: color,
     width: size,
     height: size,
     borderRadius: size / 2,
   };
-  return <div style={style}></div>;
+  return (
+    <div style={style} className={className}>
+      {children}
+    </div>
+  );
 };
 
 export default Circle;
