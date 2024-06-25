953d89e1c0c2d92a509b9a0f9cf762250832e75a
catmaker <rzt0317@nate.com>
2024-06-25 14:53:46 +0900

    🎨 sideBar 관련 코드 제거 및 구조 개선선

--- src/app/(root)/home/HomeClient.tsx
+++ src/app/(root)/home/HomeClient.tsx
@@ -2,7 +2,6 @@
 import React, { useEffect, useContext } from "react";
 import { UserContext } from "@/app/provider/UserProvider";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
 import Image from "next/image";
 // css
 import styles from "./HomeClient.module.scss";
@@ -10,14 +9,6 @@ import styles from "./HomeClient.module.scss";
 import SideBar from "@/components/ui/SideBar/SideBar";
 import SearchBar from "@/components/ui/SearchBar/SearchBar";
 import Circle from "@/components/ui/Circle";
-// svg
-import LogoIcon from "@/asset/svg/logo.svg";
-import LogoImage from "@/asset/svg/logoImg.svg";
-import HomeIcon from "@/asset/svg/home.svg";
-import LedgerIcon from "@/asset/svg/ledger.svg";
-import ScheduleIcon from "@/asset/svg/schedule.svg";
-import JournalIcon from "@/asset/svg/journal.svg";
-import SettingIcon from "@/asset/svg/setting.svg";
 
 const HomeClient = () => {
   const router = useRouter();
@@ -33,54 +24,8 @@ const HomeClient = () => {
 
   return (
     <div className={styles.container}>
-      <SideBar align="center">
-        <aside>
-          <section>
-            <div className={styles.logo_container}>
-              <div className={styles.logo_image}>
-                <LogoImage />
-              </div>
-              <div className={styles.logo_icon}>
-                <LogoIcon />
-              </div>
-            </div>
-            <h3 className={styles.overview_title}>OVERVIEW</h3>
-            <ul className={styles.menu_list}>
-              <li className={styles.menu_item}>
-                <Link href={"/home"}>
-                  <HomeIcon />
-                  <strong className={styles.menu_text}>Home</strong>
-                </Link>
-              </li>
-              <li className={styles.menu_item}>
-                <Link href={"/ledger"}>
-                  <LedgerIcon />
-                  <strong className={styles.menu_text}>Ledger</strong>
-                </Link>
-              </li>
-              <li className={styles.menu_item}>
-                <Link href={"/schedule"}>
-                  <ScheduleIcon />
-                  <strong className={styles.menu_text}>Schedule</strong>
-                </Link>
-              </li>
-              <li className={styles.menu_item}>
-                <Link href={"/journal"}>
-                  <JournalIcon />
-                  <strong className={styles.menu_text}>Journal</strong>
-                </Link>
-              </li>
-              <li className={styles.menu_item}>
-                <Link href={"/setting"}>
-                  <SettingIcon />
-                  <strong className={styles.menu_text}>Setting</strong>
-                </Link>
-              </li>
-            </ul>
-          </section>
-        </aside>
-      </SideBar>
       <header className={styles.header}>
+        <SideBar />
         <section className={styles.search_group}>
           <SearchBar
             width="850px"
