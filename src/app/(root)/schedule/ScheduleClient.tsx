9c49c3e2ffe8fc40bae6a96cfe362c881bb2c760
catmaker <rzt0317@nate.com>
2024-07-06 21:07:35 +0900

    💄 모션 애니메이션 추가

--- src/app/(root)/schedule/ScheduleClient.tsx
+++ src/app/(root)/schedule/ScheduleClient.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useContext, useEffect, useState } from "react";
-import Image from "next/image";
+import { motion } from "framer-motion";
 // components
 import Calendar from "@/components/ui/calendar/Calendar";
 import SideBar from "@/components/ui/SideBar/SideBar";
@@ -56,7 +56,11 @@ const ScheduleClient = () => {
   return (
     <div className={styles.container}>
       <SideBar />
-      <div
+      <motion.div
+        layout
+        initial={{ opacity: 0, scale: 0.5 }} // 초기 상태
+        animate={{ opacity: 1, scale: 1 }} // 최종 상태: 완전 불투명하고, 원래 크기
+        transition={{ duration: 0.3 }} // 애니메이션 지속 시간: 0.5초
         className={`${styles.calendar} ${isModalOpen ? styles.modalBackground : ""}`}
       >
         <Calendar onDateClick={handleDateClick} />
@@ -73,7 +77,7 @@ const ScheduleClient = () => {
           setIsModalOpen={setIsModalOpen} // 현재 모달 열림 상태 변경 함수
           setIsNextModalOpen={setIsNextModalOpen} // 다음 모달 열림 상태 변경 함수
         />
-      </div>
+      </motion.div>
     </div>
   );
 };
