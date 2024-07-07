6c27ec5f15568d10a921e7ca71fdfe68da8716f7
catmaker <rzt0317@nate.com>
2024-07-08 05:19:58 +0900

    ✨email, postId로 게시물 가져오는 api 추가 /schedule/[id] 경로

---
+++ src/app/api/user/getPostById/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getPostById } from "@/lib/firebase/firestore";
+export async function POST(request: NextRequest) {
+  const { id, email } = await request.json();
+  const post = await getPostById(id, email);
+  if (!post) {
+    return NextResponse.json(
+      { message: "포스트를 가져오는데 실패했습니다." },
+      { status: 404 },
+    );
+  }
+
+  return NextResponse.json({ status: 200, data: post });
+}
