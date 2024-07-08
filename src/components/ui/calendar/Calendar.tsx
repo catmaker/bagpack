b05f7a39d954ef4a3fc7f823d265abb2505ba76f
catmaker <rzt0317@nate.com>
2024-07-09 07:49:15 +0900

    ✨post calendar 에서 event 객체로 전달 (title, date 전달용)

--- src/components/ui/calendar/Calendar.tsx
+++ src/components/ui/calendar/Calendar.tsx
@@ -2,20 +2,42 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 import interactionPlugin from "@fullcalendar/interaction"; // 필요한 플러그인!
+// zustand
+import useScheduleStore from "@/store/schedule";
+// styles
+import styles from "./Calendar.module.scss";
 type CalendarProps = {
   onDateClick?: (info: any) => void;
   event?: [];
 };
+type Event = {
+  title: string;
+  date: string;
+  backgroundColor: string;
+};
 
-const Calendar = ({ onDateClick, event }: CalendarProps) => {
+const Calendar = ({ onDateClick }: CalendarProps) => {
+  let event: Event[] = []; // 'event' 변수에 명시적인 타입 선언
+  const posts = useScheduleStore((state) => state.posts);
+  posts.map((post) => {
+    event.push({
+      title: post.title,
+      date: post.startDate,
+      backgroundColor: "red",
+    });
+  });
+
   return (
-    <FullCalendar
-      plugins={[dayGridPlugin, interactionPlugin]}
-      initialView="dayGridMonth"
-      selectable={true} // 날짜 선택 가능
-      dateClick={onDateClick} // 날짜 클릭 시 이벤트
-      events={event}
-    />
+    <div className={styles.calendar}>
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        selectable={true} // 날짜 선택 가능
+        select={onDateClick} // 날짜 클릭 시 이벤트
+        events={event}
+        height={"100%"}
+      />
+    </div>
   );
 };
 
