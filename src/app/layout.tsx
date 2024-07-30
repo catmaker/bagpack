aa6dd22a0252c06ca80bb126fda5b944b8a5ae9c
catmaker <rzt0317@nate.com>
2024-07-30 23:48:29 +0900

    💄favicon 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import "./reset.css";
 import "./globals.css";
 export const metadata: Metadata = {
-  title: "bagPack",
+  title: "TimeInk",
   description: "당신의 인생 여정을 기록하는 특별한 공간",
+  icons: "/bagpackIcon/favicon.png",
 };
 import { UserProvider } from "@/app/provider/UserProvider";
 export default function RootLayout({
