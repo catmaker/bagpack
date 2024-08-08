6622038b212f4c87b03dfde57cf3d56c3de19872
catmaker <rzt0317@nate.com>
2024-08-08 14:23:55 +0900

    💄 잡다한 디자인 수정

--- src/app/containers/intro/WelcomeSection.tsx
+++ src/app/containers/intro/WelcomeSection.tsx
@@ -3,20 +3,16 @@ import styles from "./index.module.scss";
 import { Note } from "../../../../public/svg";
 const WelcomeSection = () => {
   return (
-    <div>
-      <section className={styles.firstSection}>
-        <Note width={250} height={250} />
-        <div className={styles.firstSectionInfo}>
-          <h1 className={styles.sectionTitle}>
-            TimeInK에 오신 것을 환영합니다.
-          </h1>
-          <p className={styles.sectionContent}>
-            여행의 모든 순간을 기록하세요! 우리의 플랫폼에서 일정 관리와 여행
-            일지를 한 곳에서 간편하게 작성할 수 있습니다.
-          </p>
-        </div>
-      </section>
-    </div>
+    <section className={styles.firstSection}>
+      <Note width={250} height={250} />
+      <div className={styles.firstSectionInfo}>
+        <h1 className={styles.sectionTitle}>TimeInK에 오신 것을 환영합니다.</h1>
+        <p className={styles.sectionContent}>
+          여행의 모든 순간을 기록하세요! 우리의 플랫폼에서 일정 관리와 여행
+          일지를 한 곳에서 간편하게 작성할 수 있습니다.
+        </p>
+      </div>
+    </section>
   );
 };
 
