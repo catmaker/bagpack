5b25e2251ed8691e3973d73eef9adbd907129a5e
catmaker <rzt0317@nate.com>
2024-08-30 18:46:29 +0900

    ♻️ zustand 관련 type 분리

---
+++ src/types/store.ts
@@ -0,0 +1,11 @@
+export type Post = {
+  id: string;
+  content?: string;
+  mood?: string;
+  title?: string;
+  startDate: string;
+  endDate: string;
+};
+export type User = {
+  email: string;
+};
