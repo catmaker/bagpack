f69bc47f695366e9e978aaebd222c4c37e9d0724
catmaker <rzt0317@nate.com>
2024-08-30 18:47:01 +0900

    ⚡️useDateManagement 커스텀 훅 생성

---
+++ src/hooks/useDateManagement.ts
@@ -0,0 +1,47 @@
+import { useEffect } from "react";
+import useScheduleStore from "@/store/schedule";
+
+export const useDateManagement = () => {
+  const {
+    selectedDate,
+    setSelectedDate,
+    startDate,
+    setStartDate,
+    endDate,
+    setEndDate,
+  } = useScheduleStore((state) => ({
+    selectedDate: state.selectedDate,
+    setSelectedDate: state.setSelectedDate,
+    startDate: state.startDate,
+    setStartDate: state.setStartDate,
+    endDate: state.endDate,
+    setEndDate: state.setEndDate,
+  }));
+
+  useEffect(() => {
+    if (selectedDate) {
+      setStartDate(selectedDate);
+      setEndDate(selectedDate);
+    }
+  }, [selectedDate, setStartDate, setEndDate]);
+
+  const handleStartDateChange = (date: Date | null) => {
+    setStartDate(date || new Date());
+    setSelectedDate(date);
+    if (endDate && date && date > endDate) {
+      setEndDate(date);
+    }
+  };
+
+  const handleEndDateChange = (date: Date | null) => {
+    setEndDate(date || new Date());
+  };
+
+  return {
+    selectedDate,
+    startDate,
+    endDate,
+    handleStartDateChange,
+    handleEndDateChange,
+  };
+};
