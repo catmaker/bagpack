15cbdb5c2ead61e4a6fca1cbb80b3a22ca58d491
catmaker <rzt0317@nate.com>
2024-09-01 13:24:06 +0900

    ♻️ 홈 페이지 컴포넌트 리팩토링 및 미들웨어 활용
    - 클라이언트 사이드 라우팅 로직 제거
    - 미들웨어를 통한 인증 상태 확인으로 변경
    - 불필요한 상태 및 useEffect 제거
    - 컴포넌트 로직 간소화

--- src/app/containers/home/index.tsx
+++ src/app/containers/home/index.tsx
@@ -1,35 +1,21 @@
 "use client";
 
-import React, { useEffect, useContext, useState } from "react";
-import { useRouter } from "next/navigation";
+import React, { useContext } from "react";
 import { UserContext } from "@/app/provider/UserProvider";
-import Loading from "@/components/Loading";
 import Header from "./Header";
 import styles from "./index.module.scss";
 
 const HomeClient: React.FC = () => {
-  const router = useRouter();
   const user = useContext(UserContext);
-  const [isLoading, setIsLoading] = useState(true); // 로딩 상태 추가
-
-  useEffect(() => {
-    if (user !== null) {
-      setIsLoading(false);
-    } else {
-      router.push("/login");
-    }
-  }, [user, router]);
-
-  if (isLoading) {
-    return <Loading />; // 로딩 중일 때 Loading 컴포넌트 표시
+  if (!user) {
+    return null; // 또는 로딩 컴포넌트를 반환
   }
 
   return (
-    user && (
-      <div className={styles.homeContainer}>
-        <Header user={user} />
-      </div>
-    )
+    <div className={styles.homeContainer}>
+      <Header user={user} />
+      {/* 홈 페이지의 나머지 내용 */}
+    </div>
   );
 };
 
