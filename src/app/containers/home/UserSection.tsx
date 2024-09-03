015bb3336bb571a812f52a262f2b94a06ffaebdc
catmaker <rzt0317@nate.com>
2024-09-03 23:01:11 +0900

    💄 UserSection UI 개선 및 기능 단순화
    - 프로필 이미지 크기 확대 (38px → 300px)
    - 알람 아이콘 제거
    - 'Edit Profile' 링크 추가 (/mypage로 연결)
    
    🔥 불필요한 요소 제거
    - AlarmIcon 컴포넌트 및 관련 스타일 제거
    
    ♻️ 컴포넌트 구조 단순화
    - userInfoWrapper 및 userProfileContainer div 제거
    
    ✨ 새로운 기능 추가
    - 프로필 편집 페이지로 이동하는 링크 구현
    
    🎨 전반적인 레이아웃 및 스타일 변경

--- src/app/containers/home/UserSection.tsx
+++ src/app/containers/home/UserSection.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import AlarmIcon from "@/asset/svg/alarm.svg";
+import Link from "next/link";
 import Circle from "@/components/ui/Circle";
 import { User } from "@/types/user";
 import styles from "./UserSection.module.scss";
@@ -7,22 +7,18 @@ import styles from "./UserSection.module.scss";
 const UserSection = ({ user }: { user: User }) => {
   return (
     <section className={styles.userSectionContainer}>
-      <div className={styles.userInfoWrapper}>
-        <Circle className={styles.alarmIconWrapper} color="white" size={38}>
-          <AlarmIcon width="16" height="16" fill="rgb(255, 119, 119)" />
-        </Circle>
-        <div className={styles.userProfileContainer}>
-          <Circle className={styles.avatarWrapper} color="white" size={38}>
-            <Image
-              src="/bagpackIcon/profile.jpg"
-              fill
-              alt="userProfile"
-              style={{ borderRadius: "50%" }}
-            />
-          </Circle>
-          <p className={styles.userNickname}>{user?.nickname}</p>
-        </div>
-      </div>
+      <Circle className={styles.avatarWrapper} color="white" size={300}>
+        <Image
+          src="/bagpackIcon/profile.jpg"
+          fill
+          alt="userProfile"
+          style={{ borderRadius: "50%" }}
+        />
+      </Circle>
+      <p className={styles.userNickname}>{user?.nickname}</p>
+      <Link href="/mypage" className={styles.editProfileLink}>
+        Edit Profile
+      </Link>
     </section>
   );
 };
