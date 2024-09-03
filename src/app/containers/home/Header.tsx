3945e84d48e381e5c1078c3e5e73ec037ceaf4a5
catmaker <rzt0317@nate.com>
2024-09-03 23:08:42 +0900

    🔥 Header 컴포넌트 정리 및 불필요한 import 제거
    - 사용하지 않는 import 문 제거:
      - motion (framer-motion)
      - SideBar
      - SearchSection
      - UserSection
    
    🐛 빌드 오류 수정
    - 사용하지 않는 import로 인한 빌드 실패 해결

--- src/app/containers/home/Header.tsx
+++ src/app/containers/home/Header.tsx
@@ -1,9 +1,5 @@
-import { motion } from "framer-motion";
 import styles from "@/app/containers/home/Header.module.scss";
-import SideBar from "@/components/ui/SideBar/SideBar";
 import { User } from "@/types/user";
-import SearchSection from "./SearchSection";
-import UserSection from "./UserSection";
 
 const Header = ({ user }: { user: User }) => {
   return <header className={styles.header} />;
