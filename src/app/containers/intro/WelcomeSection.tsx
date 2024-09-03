d2ec920896c7d8a01cb22aeb428709cf5ad46458
catmaker <rzt0317@nate.com>
2024-09-03 22:45:13 +0900

    🔥 WelcomeSection에서 대용량 Note SVG 제거
    💄 CSS로 SVG 대체하여 UI 유지
    
    🚀 페이지 로딩 속도 개선
    
    ♻️ 불필요한 이미지 리소스 정리

--- src/app/containers/intro/WelcomeSection.tsx
+++ src/app/containers/intro/WelcomeSection.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { Note } from "../../../../public/svg";
 import styles from "./WelcomeSection.module.scss";
 
 const WelcomeSection = () => {
   return (
     <section className={styles.welcomeSection}>
-      <Note width={250} height={250} className={styles.welcomeIcon} />
       <div className={styles.welcomeContent}>
         <h1 className={styles.welcomeTitle}>TimeInK에 오신 것을 환영합니다.</h1>
         <p className={styles.welcomeDescription}>
