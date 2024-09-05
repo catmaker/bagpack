65254ec7335b0add3479fc4e913391ec811539ac
catmaker <rzt0317@nate.com>
2024-09-06 00:31:00 +0900

    ✨ 기능: Home 페이지 레이아웃 개선 및 WelcomeSection 추가
    - Home 페이지에 WelcomeSection 컴포넌트 추가
    - UserSection과 WelcomeSection을 Card 컴포넌트로 감싸 디자인 통일성 향상
    - MainSection도 Card 컴포넌트로 감싸 일관된 UI 제공
    - 전체적인 페이지 구조 개선으로 사용자 경험(UX) 향상

--- src/app/containers/home/index.tsx
+++ src/app/containers/home/index.tsx
@@ -2,9 +2,10 @@
 
 import React, { useContext } from "react";
 import { UserContext } from "@/app/provider/UserProvider";
-import Header from "@/components/ui/header/Header";
+import Card from "@/components/ui/Card";
 import MainSection from "./MainSection";
 import UserSection from "./UserSection";
+import WelcomeSection from "./WelcomeSection";
 import styles from "./index.module.scss";
 
 const HomeClient: React.FC = () => {
@@ -13,13 +14,25 @@ const HomeClient: React.FC = () => {
     return null; // 또는 로딩 컴포넌트를 반환
   }
   return (
-    <>
-      <Header />
-      <div className={styles.homeContainer}>
+    <div className={styles.homeContainer}>
+      <Card
+        width="80%"
+        height="100%"
+        className={`${styles.homeHeader} ${styles.card}`}
+        boxShadow="0px 0px 10px 0px rgba(0, 0, 0, 0.1)"
+      >
         <UserSection user={user} />
+        <WelcomeSection user={user} />
+      </Card>
+      <Card
+        width="80%"
+        height="100%"
+        className={`${styles.homeMain} ${styles.card}`}
+        boxShadow="0px 0px 10px 0px rgba(0, 0, 0, 0.1)"
+      >
         <MainSection user={user} />
-      </div>
-    </>
+      </Card>
+    </div>
   );
 };
 
