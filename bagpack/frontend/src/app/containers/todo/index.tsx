48ac33f29202e0824281656e82bbd6a14fb48347
catmaker <rzt0317@nate.com>
2024-09-20 16:45:50 +0900

    🐛todolist 무한로딩 해결

--- bagpack/frontend/src/app/containers/todo/index.tsx
+++ bagpack/frontend/src/app/containers/todo/index.tsx
@@ -8,7 +8,6 @@ const Todo = () => {
   const [inputValue, setInputValue] = useState("");
   const [todoItems, setTodoItems] = useState<string[]>([]);
   const [lastCheckedDate, setLastCheckedDate] = useState<string>("");
-  const [isLoading, setIsLoading] = useState<boolean>(true);
   useEffect(() => {
     const checkAndLoadTodos = () => {
       const currentDate = new Date().toLocaleDateString();
@@ -24,11 +23,9 @@ const Todo = () => {
         try {
           const parsedTodos = JSON.parse(localTodo);
           setTodoItems(parsedTodos);
-          setIsLoading(false);
         } catch (error) {
           console.error("Failed to parse todo items:", error);
           setTodoItems([]);
-          setIsLoading(false);
         }
       }
     };
@@ -63,44 +60,35 @@ const Todo = () => {
         주의: 이 TODOLIST는 하루 일정에 적합합니다. 날짜가 바뀌면 모든 항목이
         삭제됩니다.
       </p>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <>
-          <div className={styles.inputContainer}>
-            <input
-              className={styles.input}
-              type="text"
-              placeholder="할 일을 입력하세요"
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
-            />
-            <button
-              className={styles.button}
-              onClick={handleAddTodoLocalStorage}
-            >
-              추가
-            </button>
-          </div>
-          <ul className={styles.todoList}>
-            {todoItems.length > 0 ? (
-              todoItems.map((item, index) => (
-                <li key={`todo-${index}`} className={styles.todoItem}>
-                  <span>{item}</span>
-                  <button
-                    className={styles.deleteButton}
-                    onClick={() => handleDeleteTodo(index)}
-                  >
-                    삭제
-                  </button>
-                </li>
-              ))
-            ) : (
-              <li className={styles.emptyList}>할 일이 없습니다.</li>
-            )}
-          </ul>
-        </>
-      )}
+      <div className={styles.inputContainer}>
+        <input
+          className={styles.input}
+          type="text"
+          placeholder="할 일을 입력하세요"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button className={styles.button} onClick={handleAddTodoLocalStorage}>
+          추가
+        </button>
+      </div>
+      <ul className={styles.todoList}>
+        {todoItems.length > 0 ? (
+          todoItems.map((item, index) => (
+            <li key={`todo-${index}`} className={styles.todoItem}>
+              <span>{item}</span>
+              <button
+                className={styles.deleteButton}
+                onClick={() => handleDeleteTodo(index)}
+              >
+                삭제
+              </button>
+            </li>
+          ))
+        ) : (
+          <li className={styles.emptyList}>할 일이 없습니다.</li>
+        )}
+      </ul>
     </div>
   );
 };
